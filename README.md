e307dab066e13f2c596e9f59d0f4a0c18d1cfc06
Lauren Bichelmeir <l.bichelmeir@gmail.com>
2021-03-30 14:05:48 +0800

    implemented basic quote genneration with api

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # quote-generator-javascript
 a random quote generator built with HTML/CSS/JavaScript
+
+
+## Learning Outcomes [Why]
+I wanted to further my vanilla javascript knowledge, so I decided to create a random quote generator! 
+
+### Learning Outcomes
+[] practice using an API
+[] practice HTML & CSS styling
+
+## What I learned
+- Figuring out how to use the quotes API wasn't as hard as I thought it would be, so I thought I would challenge myself by trying to generate random background images using the **Unsplash API**
+
---
+++ quote-generator/app.js
@@ -0,0 +1,52 @@
+// https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
+
+const url = "https://api.quotable.io/random";
+const img_url = "https://api.unsplash.com/photos/random/?client_id=DCEdsBlifmBj14jUCoocVR3f_R_jZy-Zcl3S5IoiuEk"
+// const img_url = "https://api.unsplash.com/photos/random/?client_id=DCEdsBlifmBj14jUCoocVR3f_R_jZy-Zcl3S5IoiuEk&query=nature&orientation=landscape"
+// const img_url = "https://api.unsplash.com/photos/random/?client_id=DCEdsBlifmBj14jUCoocVR3f_R_jZy-Zcl3S5IoiuEk&collection=3656341"
+
+
+// url('https://source.unsplash.com/collection/1240/1600x900');
+
+function newQuote() {
+fetch(url)
+  .then(function(data) {
+    return data.json();
+  })
+  .then(function(data) {
+      document.getElementById('quote').innerHTML = data.content;
+      document.getElementById('author').innerHTML = "- "+ data.author
+    // document.querySelector(".author").innerHTML = "- " + data.author;
+    console.log(data);
+  });
+
+}
+
+function newBackground() {
+    fetch(img_url)
+        .then(function(imgdata) {
+            return imgdata.json();
+
+        })
+        .then(function(imgdata) {
+          document.body.style.backgroundImage = 'url('+imgdata.urls.full+')';
+            // document.getElementById('background').src = imgdata.urls.regular;
+
+        })
+    }
+    // document.getElementById('background').src = "https://source.unsplash.com/collection/1240/1600x900"
+
+
+
+// {/* <img src="https://source.unsplash.com/user/erondu/1600x900"></img> */}
+// https://source.unsplash.com/collection/1240/1600x900
+
+
+// omponentDidMount() {
+//     fetch("https://api.imgflip.com/get_memes")
+//         .then(response => response.json())
+//         .then(response => {
+//             const {memes} = response.data
+//             this.setState({ allMemeImgs: memes })
+//         })
+// }
