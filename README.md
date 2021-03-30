a496508bcf13376de38cfcc9bbc8e2f9f0010983
Lauren Bichelmeir <l.bichelmeir@gmail.com>
2021-03-30 20:10:12 +0800

    cleaned up code and updated readme

--- README.md
+++ README.md
@@ -3,13 +3,19 @@ a random quote generator built with HTML/CSS/JavaScript
 
 ![](WisdomGenerator.gif)
 
-## Learning Outcomes [Why]
-I wanted to further my vanilla javascript knowledge, so I decided to create a random quote generator! 
+## What I did
+I wanted to further my vanilla javascript knowledge, so I decided to create a **random quote generator**! 
 
 ### Learning Outcomes
-[] practice using an API
-[] practice HTML & CSS styling
+- [X] practice using an API 
+- [X] practice HTML & CSS styling
 
 ## What I learned
-- Figuring out how to use the quotes API wasn't as hard as I thought it would be, so I thought I would challenge myself by trying to generate random background images using the **Unsplash API**
+- Figuring out how to use the **quotes API** wasn't as hard as I thought it would be, so I thought I would challenge myself by trying to generate random background images using the **Unsplash API**
+- I had some trouble figuring out how to 'fetch' the data from the Unsplash API as I couldn't figure out what URL to use and the syntax for specifying parameters. It took a while but I was able to do it! My current parameters are 'nature wallpaper' and 'landscape orientation'
+- At one point the button to generate a new image stopped working and I was so confused, until I realized that the API allows for only 50 requests an hour...
+
+### The Wisdom Generator Features
+- button to generate a new quote (from the API quotable)
+- button to generate a different background image (thanks Unsplash!)
 
--- quote-generator/app.js
+++ quote-generator/app.js
@@ -1,5 +1,3 @@
-// https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
-
 const url = "https://api.quotable.io/random";
 // const img_url = "https://api.unsplash.com/photos/random/?client_id=DCEdsBlifmBj14jUCoocVR3f_R_jZy-Zcl3S5IoiuEk"
 const img_url = "https://api.unsplash.com/photos/random/?client_id=DCEdsBlifmBj14jUCoocVR3f_R_jZy-Zcl3S5IoiuEk&query=nature-wallpaper&orientation=landscape"
@@ -16,7 +14,6 @@ fetch(url)
   .then(function(data) {
       document.getElementById('quote').innerHTML = data.content;
       document.getElementById('author').innerHTML = "- "+ data.author +" -"
-    // document.querySelector(".author").innerHTML = "- " + data.author;
     console.log(data);
   });
 
@@ -30,23 +27,9 @@ function newBackground() {
         })
         .then(function(imgdata) {
           document.body.style.backgroundImage = 'url('+imgdata.urls.full+')';
-            // document.getElementById('background').src = imgdata.urls.regular;
 
         })
     }
-    // document.getElementById('background').src = "https://source.unsplash.com/collection/1240/1600x900"
 
 
-
-// {/* <img src="https://source.unsplash.com/user/erondu/1600x900"></img> */}
-// https://source.unsplash.com/collection/1240/1600x900
-
-
-// omponentDidMount() {
-//     fetch("https://api.imgflip.com/get_memes")
-//         .then(response => response.json())
-//         .then(response => {
-//             const {memes} = response.data
-//             this.setState({ allMemeImgs: memes })
-//         })
-// }
+// https://forum.freecodecamp.org/t/free-api-inspirational-quotes-json-with-code-examples/311373
